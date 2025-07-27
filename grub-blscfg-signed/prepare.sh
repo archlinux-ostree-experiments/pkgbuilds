5ac1b5007dd0f8a94533366e568f92998523698d
Marco Hünseler <marco@wuthoehle.de>
2025-07-27 17:20:15 +0200

    [grub-blscfg-signed] use mok key from filesystem

--- grub-blscfg-signed/prepare.sh
+++ grub-blscfg-signed/prepare.sh
@@ -6,10 +6,6 @@ echo "Install grub-blscfg..."
 pwd
 sudo pacman -U --noconfirm ../output/grub-blscfg-2*.pkg.tar.zst
 
-if [ -z "$SBSIGNKEY" ]; then
-    echo "ERROR: MOK Key/\$SBSIGNKEY doesn't seem to contain a key!"
-    exit 1
+if [ -f ../MOK.key ]; then
+    cp -v ../MOK.key .
 fi
-
-echo "Write MOK.key..."
-echo "$SBSIGNKEY" > MOK.key
